789dbf6fcc0e55990e2114281802f2785a165c09
Unknown <centarox@gmail.com>
2017-12-14 16:05:23 -0800

    Isolated xbee test.

--- tests/classes/cpp/xbee/xbee_test.cc
+++ tests/classes/cpp/xbee/xbee_test.cc
@@ -52,70 +52,128 @@ error_t PingCallback(const comnet::Header& header, const Ping& packet, comnet::C
 
 int main(int c, char** args) {
 
+
+	localTest();
+	isloatedTest();
+	
+	return 0;
+}
+
+// Test against an xbee on another machine.
+void isolateTest()
+{
+	const char* deviceMac = "0013A2004067E4AE";
+	// test date
+	std::cout << "Test: 11/17/2017" << std::endl;
+	//Disables Pinging to make reading output easier
+	comnet::constate::ConnectionStateManager::ConStateEnabled = false;
+
+	std::condition_variable cond;
+	std::cout << sizeof(comnet::Header) << std::endl;
+	// CommNode 1
+	comnet::Comms comm1(1);
+	comm1.LoadKey("01234567890ABCDEF");
+
+	comnet::architecture::os::CommMutex mut;
+	comnet::architecture::os::CommLock commlock(mut);
+	// This will cause the thread to wait for a few milliseconds, causing any other thread to wait.
+	comnet::architecture::os::WaitForMilliseconds(commlock, cond, 1000);
+
+	std::cout << "Test complete!" << std::endl;
+	// CommNode 1 init and add Connection.
+	std::cout << "Init connection succeeded: "
+		<< std::boolalpha
+		<< comm1.InitConnection(ZIGBEE_LINK, "COM5", "", 57600)
+		<< std::endl;
+	std::cout << "Connected to address: "
+		<< std::boolalpha
+		<< comm1.AddAddress(2, deviceMac)
+		<< std::endl;
+
+	comm1.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
+
+	// Test packet. 
+
+	Ping large("CAT NAME IS LILY");
+	// NOTE(All): Be sure to run the nodes! If not, the threads won't execute!
+	comm1.Run();
+
+	// Loop. To exit, Click the red button on the top left (Windows Visual Studio) OR 
+	// CNTRL+C (Linux). 
+	while (true) {
+		std::cout << "Sleeping..." << std::endl;
+		// comm1 will be sending the packet.
+		comm1.Send(large, 2);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+	std::cin.ignore();
+}
+
+
+//test two xbees on same machine.
+void localTest()
+{
 	const char* deviceMac1 = "0013A2004067E4A0";
 	const char* deviceMac2 = "0013A2004067E49F";
 
-
-
 	// test date
-	std::cout << "Test: 11/15/2017" << std::endl;
-  //Disables Pinging to make reading output easier
-  comnet::constate::ConnectionStateManager::ConStateEnabled = false;
+	std::cout << "Test: 11/17/2017" << std::endl;
+	//Disables Pinging to make reading output easier
+	comnet::constate::ConnectionStateManager::ConStateEnabled = false;
 
-  std::condition_variable cond;
-  std::cout << sizeof(comnet::Header) << std::endl;
-  // CommNode 1
-  comnet::Comms comm1(1);
-  comm1.LoadKey("01234567890ABCDEF");
-  // CommNode 2
-  comnet::Comms comm2(2);
-  comm2.LoadKey("01234567890ABCDEF");
-  comnet::architecture::os::CommMutex mut;
-  comnet::architecture::os::CommLock commlock(mut);
-  // This will cause the thread to wait for a few milliseconds, causing any other thread to wait.
-  comnet::architecture::os::WaitForMilliseconds(commlock, cond, 1000);
+	std::condition_variable cond;
+	std::cout << sizeof(comnet::Header) << std::endl;
+	// CommNode 1
+	comnet::Comms comm1(1);
+	comm1.LoadKey("01234567890ABCDEF");
+	// CommNode 2
+	comnet::Comms comm2(2);
+	comm2.LoadKey("01234567890ABCDEF");
+	comnet::architecture::os::CommMutex mut;
+	comnet::architecture::os::CommLock commlock(mut);
+	// This will cause the thread to wait for a few milliseconds, causing any other thread to wait.
+	comnet::architecture::os::WaitForMilliseconds(commlock, cond, 1000);
 
-  std::cout << "Test complete!" << std::endl;
-  // CommNode 1 init and add Connection.
-  std::cout << "Init connection succeeded: "
-    << std::boolalpha
-    << comm1.InitConnection(ZIGBEE_LINK, "COM3", "", 57600)
-    << std::endl;
-  std::cout << "Connected to address: "
-    << std::boolalpha
-	  << comm1.AddAddress(2, deviceMac1)// "0013A2004067E4AE")
-    << std::endl;
-  // ComNode 2 init and add Connection.
-  std::cout << "Init connection succeeded: "
-   << std::boolalpha
-   << comm2.InitConnection(ZIGBEE_LINK, "COM4", "", 57600)
-   << std::endl;
-  std::cout << "Connected to address: "
-   << std::boolalpha
-   << comm2.AddAddress(1, deviceMac2)//"0013A20040917A31")
-   << std::endl;
-  // CommNode 2 init and add Connection.
-  // CommNode Callback linking.
+	std::cout << "Test complete!" << std::endl;
+	// CommNode 1 init and add Connection.
+	std::cout << "Init connection succeeded: "
+		<< std::boolalpha
+		<< comm1.InitConnection(ZIGBEE_LINK, "COM3", "", 57600)
+		<< std::endl;
+	std::cout << "Connected to address: "
+		<< std::boolalpha
+		<< comm1.AddAddress(2, deviceMac1)// "0013A2004067E4AE")
+		<< std::endl;
+	// ComNode 2 init and add Connection.
+	std::cout << "Init connection succeeded: "
+		<< std::boolalpha
+		<< comm2.InitConnection(ZIGBEE_LINK, "COM4", "", 57600)
+		<< std::endl;
+	std::cout << "Connected to address: "
+		<< std::boolalpha
+		<< comm2.AddAddress(1, deviceMac2)//"0013A20040917A31")
+		<< std::endl;
+	// CommNode 2 init and add Connection.
+	// CommNode Callback linking.
 
-  comm1.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
-  comm2.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
+	comm1.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
+	comm2.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
 
 
-  // Test packet. 
-  Ping small("Cats. MEW :3");
-  Ping large("The quick brown fox jumps over a lazy dog.");
-  // NOTE(All): Be sure to run the nodes! If not, the threads won't execute!
-  comm1.Run();
-  comm2.Run();
+	// Test packet. 
+	Ping small("Cats. MEW :3");
+	Ping large("The quick brown fox jumps over a lazy dog.");
+	// NOTE(All): Be sure to run the nodes! If not, the threads won't execute!
+	comm1.Run();
+	comm2.Run();
 
-  // Loop. To exit, Click the red button on the top left (Windows Visual Studio) OR 
-  // CNTRL+C (Linux). 
-  while(true) {
-    std::cout << "Sleeping..." << std::endl;
-    // comm1 will be sending the packet.
-    comm1.Send(large, 2);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
-  std::cin.ignore();
-  return 0;
+	// Loop. To exit, Click the red button on the top left (Windows Visual Studio) OR 
+	// CNTRL+C (Linux). 
+	while (true) {
+		std::cout << "Sleeping..." << std::endl;
+		// comm1 will be sending the packet.
+		comm1.Send(large, 2);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+	std::cin.ignore();
 }
