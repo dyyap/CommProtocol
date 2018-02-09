6b5becaf4e973f42036e8663e0f9c3dbe46acf5b
Unknown <centarox@gmail.com>
2018-02-08 22:22:44 -0800

    setup test.

--- tests/classes/cpp/xbee/xbee_test.cc
+++ tests/classes/cpp/xbee/xbee_test.cc
@@ -38,7 +38,118 @@ private:
 
   std::string cat;
 };
+class ArmCommand : INHERITS_ABSPACKET {
+	/**
+	Creates an instance
+	*/
+public:
+	ArmCommand(uint8_t id = 0,
+		int32_t position = 0)
+		: CHAIN_ABSPACKET(ArmCommand),
+		id(id),
+		position(position)
+	{
+	}
 
+
+
+	/**
+	Pack data into the stream for sending out.
+	*/
+	void Pack(comnet::ObjectStream &obj) override {
+		obj << id;
+		obj << position;
+	}
+
+
+	/**
+	Unpack data back into this packet when receiving data.
+	*/
+	void Unpack(comnet::ObjectStream &obj) override {
+		obj >> position;
+		obj >> id;
+	}
+
+
+	/**
+	Tells CommProtocol how to recreate the ArmCommand packet
+	when receiving data.
+	*/
+	comnet::AbstractPacket *Create() override {
+		return new ArmCommand();
+	}
+	 const void print() const
+	{
+		std::cout << "VAL: ID: " << id << "\n Position:"
+			<< position << "\n";
+	}
+
+	/**
+	Data.
+	*/
+	uint8_t id;
+	int32_t position;
+};
+class ArmPosition : INHERITS_ABSPACKET {
+	/**
+	Creates an instance
+	*/
+public:
+	ArmPosition(int32_t position1 = 1,
+		int32_t position2=2,
+		int32_t position3=3,
+		int32_t position4=4)
+		: CHAIN_ABSPACKET(ArmPosition),
+		position1(position1),
+		position2(position2),
+		position3(position3),
+		position4(position4)
+	{
+	}
+
+
+	/**
+	Pack data into the stream for sending out.
+	*/
+	void Pack(comnet::ObjectStream &obj) override {
+		obj << position1;
+		obj << position2;
+		obj << position3;
+		obj << position4;
+	}
+
+
+	/**
+	Unpack data back into this packet when receiving data.
+	*/
+	void Unpack(comnet::ObjectStream &obj) override {
+		obj >> position4;
+		obj >> position3;
+		obj >> position2;
+		obj >> position1;
+	}
+
+	const void print() const
+	{
+		std::cout << "VAL: p1: " << position1 << "\n p2:" 
+			<< position2 << "\n p3:" << position3 << "\n p4" << position4 << "\n";
+	}
+	/**
+	Tells CommProtocol how to recreate the ArmPosition packet
+	when receiving data.
+	*/
+	comnet::AbstractPacket *Create() override {
+		return new ArmPosition(0, 0, 0, 0);
+	}
+
+	/**
+	Data.
+	*/
+	int32_t position1;
+	int32_t position2;
+	int32_t position3;
+	int32_t position4;
+};
 
 // Callback function that we will be using to link to Ping packet.
 error_t PingCallback(const comnet::Header& header, const Ping& packet, comnet::Comms& node) {
@@ -47,6 +158,24 @@ error_t PingCallback(const comnet::Header& header, const Ping& packet, comnet::C
   std::cout << "Message: " << std::endl;
   std::cout << "Packet contains: " << packet.GetCat() << std::endl;
   return comnet::CALLBACK_SUCCESS | comnet::CALLBACK_DESTROY_PACKET;
+}
+
+error_t ArmCommandCallback(const comnet::Header& header, const ArmCommand& packet, comnet::Comms& node) {
+	std::cout << "=::RECEIVED PACKET::=" << std::endl;
+	std::cout << std::endl << "Source node: " << (int32_t)header.source_id << std::endl;
+	std::cout << "Message: " << std::endl;
+	std::cout << "Packet contains: "; 
+	packet.print();
+	return comnet::CALLBACK_SUCCESS | comnet::CALLBACK_DESTROY_PACKET;
+}
+
+error_t ArmPositionCallback(const comnet::Header& header, const ArmPosition& packet, comnet::Comms& node) {
+	std::cout << "=::RECEIVED PACKET::=" << std::endl;
+	std::cout << std::endl << "Source node: " << (int32_t)header.source_id << std::endl;
+	std::cout << "Message: " << std::endl;
+	std::cout << "Packet contains: ";
+	packet.print();
+	return comnet::CALLBACK_SUCCESS | comnet::CALLBACK_DESTROY_PACKET;
 }
 
 
@@ -88,6 +217,8 @@ void isolatedTest()
 	Ping holder("");
 	comm1.LinkCallback(&holder, new comnet::Callback((comnet::callback_t)PingCallback));
 
+	comm1.LinkCallback(new ArmCommand() , new comnet::Callback((comnet::callback_t)ArmCommandCallback));
+	comm1.LinkCallback(new ArmPosition(), new comnet::Callback((comnet::callback_t)ArmPositionCallback));
 	//comm1.LinkCallback(new Ping(), new comnet::Callback((comnet::callback_t)PingCallback));
 
 	// Test packet. 
@@ -116,13 +247,15 @@ void isolatedTest()
 //test two xbees on same machine.
 void localTest()
 {
-	const char* destMAC1 = "0013A2004067E4A0";
-	const char* destMAC2 = "0013A2004067E49F";
-	const char* com1PortName = "COM3";
-	const char* com2PortName = "COM4";
 	const uint8_t com1ID = 1;
 	const uint8_t com2ID = 2;
-	const uint32_t baudRate = 57600;
+	const char* com1PortName = "COM10";
+	const char* com2PortName = "COM11";
+	const char* node1_Mac = "0013A20040917A31";
+	const char* node2_Mac = "0013A2004067E4AE";
+
+
+	const uint32_t baudRate = 57600;//57600;
 
 	// test date
 	std::cout << "Test: 11/17/2017" << std::endl;
@@ -132,10 +265,10 @@ void localTest()
 	std::condition_variable cond;
 	std::cout << sizeof(comnet::Header) << std::endl;
 	// CommNode 1
-	comnet::Comms comm1(1);
+	comnet::Comms comm1(com1ID);
 	comm1.LoadKey("01234567890ABCDEF");
 	// CommNode 2
-	comnet::Comms comm2(2);
+	comnet::Comms comm2(com2ID);
 	comm2.LoadKey("01234567890ABCDEF");
 	comnet::architecture::os::CommMutex mut;
 	comnet::architecture::os::CommLock commlock(mut);
@@ -150,16 +283,16 @@ void localTest()
 		<< std::endl;
 	std::cout << "Connected to address: "
 		<< std::boolalpha
-		<< comm1.AddAddress(com2ID, destMAC2)// "0013A2004067E4AE")
+		<< comm1.AddAddress(com2ID, node2_Mac)// "0013A2004067E4AE")
 		<< std::endl;
 	// ComNode 2 init and add Connection.
 	std::cout << "Init connection succeeded: "
 		<< std::boolalpha
-		<< comm2.InitConnection(ZIGBEE_LINK, com1PortName, "", baudRate)
+		<< comm2.InitConnection(ZIGBEE_LINK, com2PortName, "", baudRate)
 		<< std::endl;
 	std::cout << "Connected to address: "
 		<< std::boolalpha
-		<< comm2.AddAddress(com1ID, destMAC1)//"0013A20040917A31")
+		<< comm2.AddAddress(com1ID, node1_Mac)//"0013A20040917A31")
 		<< std::endl;
 	// CommNode 2 init and add Connection.
 	// CommNode Callback linking.
@@ -180,7 +313,7 @@ void localTest()
 	while (true) {
 		std::cout << "Sleeping..." << std::endl;
 		// comm1 will be sending the packet.
-		comm1.Send(large, com2ID);
+		comm1.Send(small, com2ID);
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 	std::cin.ignore();
