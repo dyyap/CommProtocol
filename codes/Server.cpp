a0da815114fc94515d00f173d8658c131ba2b7f5
hau73oik <hau73oik@gmail.com>
2018-10-02 22:06:12 -0700

    codes folder

---
+++ codes/Server.cpp
@@ -0,0 +1,9 @@
+#ifdef _WIN3
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+#else
+  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+  #include <unistd.h> /* Needed for close() */
