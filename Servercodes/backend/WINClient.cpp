ec7bab31ce7672147ec04e30cc4d007c205698b4
dyyap <35153938+dyyap@users.noreply.github.com>
2018-10-08 21:06:47 -0700

    Create WINClient.cpp

---
+++ Servercodes/backend/WINClient.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void InitializeWinsock() {
+	// Initialize WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0) {
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
+}
+
+void main()
+{
+	string ipAddress = "127.0.0.1";			
+	// IP Address of the server
+	int port = 6960;						
+	// Listening port # on the server
+	InitializeWinsock();
+
+	// Create socket
+	SOCKET Client = socket(AF_INET, SOCK_STREAM, 0);
+	if (Client == INVALID_SOCKET)
+	{
+		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	// Fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connResult = connect(Client, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+		closesocket(Client);
+		WSACleanup();
+		return;
+	}
+
+	// Do-while loop to send and receive data
+	char buf[4096];
+	string userInput;
+
+	do
+	{
+		// Prompt the user for some text
+		cout << "> ";
+		getline(cin, userInput);
+
+		if (userInput.size() > 0)		// Make sure the user has typed in something
+		{
+			// Send the text
+			int sendResult = send(Client, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+				// Wait for response
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(Client, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+					// Echo response to console
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+			}
+		}
+
+	} while (userInput.size() > 0);
+
+	// Gracefully close down everything
+	closesocket(Client);
+	WSACleanup();
+}
