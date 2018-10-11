1022bfdf0d4fd30920773514b9379eabce4f4eef
hau73oik <hau73oik@gmail.com>
2018-10-10 20:25:40 -0700

    Add update doc

--- Servercodes/frontend/general/general-server.js
+++ Servercodes/frontend/general/general-server.js
@@ -8,7 +8,7 @@
 const net = require('net');
 
 const HOST = '127.0.0.1';
-const PORT = 6969;
+const PORT = 59000;
 
 const server = net.createServer();
 server.listen(PORT, HOST, console.log('Receiving incoming messages...'));
